133ae6a35e901e66a0a0cd2c71631e4b89154b18
AaronOmidEntezar <aoentezar@gmail.com>
2026-02-21 23:06:59 -0800

    first

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return ackermann(m - 1, 1);
+    else
+        return ackermann(m - 1, ackermann(m, n - 1));
 }
 
 int main() {
@@ -11,7 +16,7 @@ int main() {
     cout << "Enter m and n: ";
     cin >> m >> n;
 
-    cout << "Ackermann(" << m << ", " << n << ") = " 
+    cout << "Ackermann(" << m << ", " << n << ") = "
          << ackermann(m, n) << endl;
 
     return 0;
